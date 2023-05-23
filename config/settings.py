9a73e52fcaf3efa009918360470ca7fc6a9fe2de
Winston <82153241+WinTush@users.noreply.github.com>
2023-05-23 17:14:31 +0000

    feat: set DEBUG mode to False in settings.py
    Changed default DEBUG mode in settings.py from True to False to improve security.
    Allowing hosts are still provided via DJANGO_ALLOWED_HOSTS variable.

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 
 DJANGO_ENV = env.str("DJANGO_ENV", "dev")
 SECRET_KEY = env("DJANGO_SECRET_KEY")
-DEBUG = env("DJANGO_DEBUG", default=True)
+DEBUG = env("DJANGO_DEBUG", default=False)
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 
 LANGUAGE_CODE = "en-us"
